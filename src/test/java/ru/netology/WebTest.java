c82619e2a66273facdb760e9f0c282e1ea9fa50b
Elena <elenazhilina@yandex.ru>
2024-05-31 22:15:41 +0500

    First test completed

--- src/test/java/ru/netology/WebTest.java
+++ src/test/java/ru/netology/WebTest.java
@@ -1,32 +1,36 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.DataProcessingException;
 
-import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
-import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
-import static ru.netology.testmode.data.DataGenerator.getRandomLogin;
-import static ru.netology.testmode.data.DataGenerator.getRandomPassword;
+import static com.codeborne.selenide.Selenide.$;
+import static ru.netology.DataGenerator.Registration.getValidUser;
+import static ru.netology.DataGenerator.Registration.getUser;
+import static ru.netology.DataGenerator.randomLogin;
+import static ru.netology.DataGenerator.randomPwd;
+import static java.nio.channels.FileChannel.open;
 
 
 class LoginTest {
 
     @BeforeEach
     void setUp() {
-       open("http://localhost:9999");
+        open("http://localhost:9999");
     }
-
 
     @Test
     @DisplayName("Successfully login with valid active user")
     void successLoginIfUserValidActive() {
-        var registeredUser = getRegisteredUser("active");
-        // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
-        //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
-        //  пользователя registeredUser
+        var validUser = getValidUser("active");
+        $("[data-test-id='login'] input").setValue(validUser.getLogin());
+        $("[data-test-id='password'] input").setValue(validUser.getPassword());
+        $("button.button").click();
+        $("h2").shouldHave(Condition.exactText("Личный кабинет")).shouldBe(Condition.visible);
     }
+
 
     @Test
     @DisplayName("Should get error message if login with not registered user")
