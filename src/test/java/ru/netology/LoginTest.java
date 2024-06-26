093b7121aa7f8e635a13cf480ec5da3da0831295
Elena <elenazhilina@yandex.ru>
2024-06-26 20:46:12 +0500

    Allure

--- src/test/java/ru/netology/LoginTest.java
+++ src/test/java/ru/netology/LoginTest.java
@@ -4,9 +4,12 @@ import com.codeborne.selenide.Condition;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
+import com.codeborne.selenide.impl.SelenideElementListIterator;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+
 import java.time.Duration;
 import static ru.netology.DataGenerator.Registration.getValidUser;
 import static ru.netology.DataGenerator.Registration.getUser;
@@ -21,6 +24,16 @@ public class LoginTest {
         open("http://localhost:9999");
     }
 
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @Test
     @DisplayName("Successfully login with valid active user")
     void successLoginIfUserValidActive() {
@@ -28,7 +41,7 @@ public class LoginTest {
         $("input[name='login']").setValue(validUser.getLogin());
         $("input[name='password']").setValue(validUser.getPassword());
         $("button[data-test-id='action-login']").click();
-        $("h2").shouldHave(Condition.exactText("Личный кабинет")).shouldBe(Condition.visible);
+        $("h2").shouldHave(Condition.exactText("Личный кабине")).shouldBe(Condition.visible);
     }
 
     @Test
